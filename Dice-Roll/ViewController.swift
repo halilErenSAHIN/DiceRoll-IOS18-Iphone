10fbcddfc054e1e3157d86abcd80b18d360548f8
Halil Eren Şahin <halilerensahin@gmail.com>
2025-03-31 19:22:57 +0300

    Initial Commit

---
+++ Dice-Roll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice-Roll
+//
+//  Created by Eren on 31.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Dice-RollTests/Dice_RollTests.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_RollTests.swift
+//  Dice-RollTests
+//
+//  Created by Eren on 31.03.2025.
+//
+
+import Testing
+@testable import Dice_Roll
+
+struct Dice_RollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
