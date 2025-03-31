a4a7546fcc6325990ce7b9e55a9e904cabad6201
Halil Eren Şahin <halilerensahin@gmail.com>
2025-03-31 20:50:47 +0300

    First Commit

--- Dice-Roll/ViewController.swift
+++ Dice-Roll/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceOne")
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceOne")
+        
     }
 
 
