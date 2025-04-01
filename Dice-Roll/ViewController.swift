47a53af8952fe26c118fcee73403577a95d70906
Halil Eren Şahin <halilerensahin@gmail.com>
2025-04-01 23:50:50 +0300

    Dice upper and lower counter.

--- Dice-Roll/ViewController.swift
+++ Dice-Roll/ViewController.swift
@@ -9,19 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceOne")
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceOne")
-        //trying normal push 2
         
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        
+        diceImageView1.image = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix")] [leftDiceNumber]
+        
+        diceImageView2.image = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix")] [rightDiceNumber]
+        
+        leftDiceNumber = leftDiceNumber + 1
+        rightDiceNumber = rightDiceNumber - 1
+        
+    }
+    
 }
 
